5fd96e3261767f40b14aea960d56a155f1b9d5f0
Neelesh kumar meena <143541329+Neelesh19634@users.noreply.github.com>
2026-09-07 23:22:59 +0530

    Time: 0 ms (100%), Space: 11.8 MB (55.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,26 +1,28 @@
 class Solution {
     private:
-    int solve(int i,int j,vector<vector<int>> &mat,vector<vector<int>> &dp){
-        if(i==0 && j==0){
-            if(mat[i][j]!=1) return 1;
-            return 0;
-        };
+    int solve(int i,int j,vector<vector<int>> &dp,vector<vector<int>> &mat){
         if(i<0 || j<0) return 0;
+
+        if(i==0 && j==0){
+            if(mat[i][j]==1) return 0;
+            else return 1;
+        }
         if(mat[i][j]==1) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
 
-        int up=solve(i-1,j,mat,dp);
+        int left=solve(i-1,j,dp,mat);
+        int up=solve(i,j-1,dp,mat);
 
-        int left=solve(i,j-1,mat,dp);
+        return dp[i][j]=left+up;
 
-        return dp[i][j]=up+left;
     }
-public:
+    public:
     int uniquePathsWithObstacles(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,mat,dp);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        return solve(n - 1, m - 1, dp, mat);
     }
 };
