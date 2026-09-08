a41c9fca5afabafc232dea86ac278e739c09971e
Neelesh kumar meena <143541329+Neelesh19634@users.noreply.github.com>
2026-09-08 23:24:49 +0530

    Time: 28 ms (61.11%), Space: 18.9 MB (61.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,15 +3,13 @@ class Solution {
     int solve(vector<int> &coins,int tar,vector<int> &dp){
         if(tar==0) return 0;
         if(tar<0) return -1;
-
         if(dp[tar]!=-2) return dp[tar];
 
         int mina=INT_MAX;
         for(auto &it:coins){
             int res=solve(coins,tar-it,dp);
-
-            if(res>=0 && res<mina){
-                mina=1+res;
+            if (res != -1) {
+                mina = min(mina, res + 1);
             }
         }
 
@@ -21,6 +19,7 @@ class Solution {
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
+     
         vector<int> dp(amount+1,-2);
 
         return solve(coins,amount,dp);
