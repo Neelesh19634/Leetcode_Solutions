ff9dda3b86b7f5a881fcf84fac518044b471cea7
Neelesh kumar meena <143541329+Neelesh19634@users.noreply.github.com>
2026-09-08 23:33:06 +0530

    Time: 3 ms (86.13%), Space: 15.4 MB (51.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,35 +1,38 @@
 class Solution {
     private:
-        int solve(int i,int tar,vector<int> &nums,vector<vector<int>> &dp){
-           if(i==0){
+    int solve(int i,int tar,vector<int> &nums,vector<vector<int>> &dp){
+        if(i==0){
             if(tar==0 && nums[i]==tar) return 2;
             if(tar==0 || nums[i]==tar) return 1;
-            return 0;
-           }
-            if(dp[i][tar]!=-1) return dp[i][tar];
-
-            int nt=solve(i-1,tar,nums,dp);
-            int t=0;
-            if(nums[i]<=tar){
-                t=solve(i-1,tar-nums[i],nums,dp);
-            }
-
-            return dp[i][tar]=(t+nt);
+            return 0; 
         }
+
+        if(dp[i][tar]!=-1) return dp[i][tar];
+        int nt=solve(i-1,tar,nums,dp);
+        int t=0;
+        if(nums[i]<=tar){
+            t=solve(i-1,tar-nums[i],nums,dp);
+        }
+
+        return dp[i][tar]=(t+nt);
+    }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        int res=accumulate(nums.begin(),nums.end(),0);
-        
+        int res=0;
 
-        if((res-target)%2) return 0;
+        for(auto &it:nums){
+            res+=it;
+        }
+
+        if((res-target) %2) return 0;
         if(target>res) return 0;
 
-        int ans=((res-target)/2);
-        vector<vector<int>> dp(n,vector<int>(ans+1,-1));
+        int k=(res-target)/2;
 
-        return solve(n-1,ans,nums,dp);
+        vector<vector<int>> dp(n,vector<int>(k+1,-1));
 
+        return solve(n-1,k,nums,dp);
 
     }
 };
